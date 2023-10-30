e8bb8e507a81972d004bb2ed69215ec521bd16a7
drmphobwcodes <mp.musengua@gmail.com>
2023-10-30 17:31:29 -0400

    Alert

--- nucampsite/screens/CampsiteInfoScreen.js
+++ nucampsite/screens/CampsiteInfoScreen.js
@@ -1,8 +1,165 @@
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 import RenderCampsite from '../features/campsites/RenderCampsite';
+import { toggleFavorite } from '../features/favorites/favoritesSlice';
+import { Button, Modal } from 'react-native';
+import { useState } from 'react';
+import { Rating, Input } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { postComment } from '../features/comments/commentsSlice';
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
-    return <RenderCampsite campsite={campsite} />;
+    const comments = useSelector((state) => state.comments);
+    const favorites = useSelector((state) => state.favorites);
+    const dispatch = useDispatch();
+    const [showModal, setShowModal] = useState(false);
+    const [rating, setRating] = useState(5);
+    const [author, setAuthor] = useState('');
+    const [text, setText] = useState('');
+    
+
+    const renderCommentItem = ({ item }) => {
+        return (
+            <View style={styles.commentItem}>
+                <Text style={{ fontSize: 14 }}>{item.text}</Text>
+                <Text >{item.rating}</Text>
+                <Text style={{ fontSize: 12 }}>
+                    {`-- ${item.author}, ${item.date}`}
+                </Text>
+
+                <Rating
+                imageSize={10}
+                readonly = {true}
+                startingValue={item.rating}
+                style={{ paddingVertical: 10 }}
+                />
+            </View>
+        );
+    };
+
+    const handleSubmit = () => {
+        const newComment = {
+            author: author,
+            text: text,
+            rating: rating,
+            campsiteId: campsite.id
+        };
+
+        dispatch(postComment(newComment));
+
+        console.log(newComment);
+        setShowModal(!showModal);
+    };
+
+    const resetForm = () => {
+        setRating(5);
+        setAuthor('');
+        setText('');
+    };
+
+    return (
+        <FlatList
+            data={comments.commentsArray.filter(
+                (comment) => comment.campsiteId === campsite.id
+            )}
+            renderItem={renderCommentItem}
+            keyExtractor={(item) => item.id.toString()}
+            contentContainerStyle={{
+                marginHorizontal: 20,
+                paddingVertical: 20
+            }}
+            ListHeaderComponent={
+                <>
+                    <RenderCampsite onShowModal={() => setShowModal(!showModal)}
+                        campsite={campsite}
+                        isFavorite={favorites.includes(campsite.id)}
+                        markFavorite={() => dispatch(toggleFavorite(campsite.id))}
+                    />
+                    <Text style={styles.commentsTitle}>Comments</Text>
+
+                    <Modal
+                        animationType={'slide'}
+                        transparent={false}
+                        visible={showModal}
+                        onRequestClose={() => setShowModal(false)}
+                    >
+                        <View style={styles.modal}>
+                            <Rating
+                                showRating
+                                startingValue={rating}
+                                imageSize={40}
+                                onFinishRating={(ratingValue) => setRating(ratingValue)}
+                                style={{ paddingVertical: 10 }}
+                            />
+
+                            <Input
+                                placeholder='Author'
+                                leftIcon={<Icon name='user-o' size={24} color='black' />}
+                                leftIconContainerStyle={{ paddingRight: 10 }}
+                                onChangeText={(value) => setAuthor(value)}
+                                value={author}
+                            />
+
+                            <Input
+                                placeholder='Comment'
+                                leftIcon={<Icon name='comment-o' size={24} color='black' />}
+                                leftIconContainerStyle={{ paddingRight: 10 }}
+                                onChangeText={(value) => setText(value)}
+                                value={text}
+                            />
+
+                            <View style={{ margin: 10 }}>
+                                <Button
+                                    title='Submit'
+                                    color='#5637DD'
+                                    onPress={() => {
+                                        handleSubmit();
+                                        resetForm();
+                                    }}
+                                />
+                            </View>
+                            <View style={{ margin: 10 }}>
+                                <Button
+                                    onPress={() => {
+                                        setShowModal(!showModal);
+                                        resetForm();
+                                    }}
+                                    color="#808080"
+                                    title="Cancel"
+                                />
+                            </View>
+                        </View>
+                    </Modal>
+                </>
+            }
+
+
+
+        />
+    );
 };
+
+const styles = StyleSheet.create({
+    commentsTitle: {
+        textAlign: 'center',
+        backgroundColor: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#43484D',
+        padding: 10,
+        paddingTop: 30
+    },
+    commentItem: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: '#fff'
+    },
+
+    modal: {
+        justifyContent: 'center',
+        margin: 20
+    },
+});
 
 export default CampsiteInfoScreen;
